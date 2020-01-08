53066da4fd322f52b9870d2cfc137b3511a4db73
Volodya <afork4ik@gmail.com>
2020-01-09 02:21:29 +1000

    DARKNESS IMPRISONING ME

---
+++ VzlomJo---slova/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char word[5]{ 'h', 'a', 'c', 'k', 's' };
+	cout << "your word: ";
+	for (int a = 0; a < 5; a++)
+	{
+		cout << word[a];
+	}
+	cout << "\n\n";
+	cout << "enter key ";
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> e;
+	int key[5]{ a, b, c, d, e };
+	cout << "your key: ";
+	for (int u = 0; u < 5; u++)
+	{
+		cout << key[u];
+	}
+	cout << endl;
+	int l = 0;
+	char hack = word[l];
+	cout << "cipher succesfully created: ";
+	hack += a;
+	cout << hack;
+	l++;
+	hack += b;
+	cout << hack;
+	l++;
+	hack += c;
+	cout << hack;
+	l++;
+	hack += d;
+	cout << hack;
+	l++;
+	hack += e;
+	cout << hack;
+	l++;
+	cout << endl << endl;
+	l = 0;
+	cout << "decryption succesfully ended: ";
+	bool check = 0;
+	while (check != 1)
+	{
+		if (hack == word[l])
+		{
+			cout << word[l];
+			l++;
+		}
+		else
+			hack--;
+		if (l == 5)
+		{
+			check = 1;
+			break;
+		}
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
